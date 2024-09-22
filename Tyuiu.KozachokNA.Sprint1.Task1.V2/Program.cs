571cee23f6d1220efecfcddd17906ab9605501e1
PericlesDarth <aanonimov210@gmail.com>
2024-09-22 18:06:17 +0500

    Added Task1

---
+++ Tyuiu.KozachokNA.Sprint1.Task1.V2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KozachokNA.Sprint1.Task1.V2.Lib;
+
+namespace Tyuiu.KozachokNA.Sprint1.Task1.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Козачок Н. А. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема:  Создания итогового решения по спринту                            *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Козачок Никита Андреевич | РППб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле x*y/(5+x) и печатает его на экране.      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
