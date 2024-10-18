2cbbc79cf606f101c4ce3a48968f0551eb720f82
PericlesDarth <aanonimov210@gmail.com>
2024-10-18 21:01:37 +0500

    /

---
+++ Tyuiu.KozachokNA.Sprint1.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KozachokNA.Sprint1.Task6.V11.Lib
+{
+    public class DataService: ISprint1Task6V11
+    {
+        public bool ChekeFirstLetterRepetition(string s)
+        {
+            bool foo = false;
+            char bar = s[0];
+
+            int l = s.Length;
+
+            for (int i = 0; i < l; i++)
+            {
+                if (s[i] == bar)
+                {
+                    foo = true;
+                }
+            }
+            return foo;
+        }
+    }
+}
