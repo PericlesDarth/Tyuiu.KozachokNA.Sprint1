12ad7c2cb1fc7acd49d6efa5b30b25eba1a163fc
PericlesDarth <aanonimov210@gmail.com>
2024-10-18 20:56:23 +0500

    /

---
+++ Tyuiu.KozachokNA.Sprint1.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KozachokNA.Sprint1.Task5.V3.Lib
+{
+    public class DataService: ISprint1Task5V3
+    {
+        public int Calculate(int bar)
+        {
+            int foo = (bar / 100) % 10;
+            return foo;
+        }
+    }
+}
