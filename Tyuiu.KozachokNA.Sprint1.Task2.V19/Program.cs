11c0375ad2cbe633a9cd141f42c135e76b34298b
PericlesDarth <aanonimov210@gmail.com>
2024-10-18 20:30:03 +0500

    /

--- Tyuiu.KozachokNA.Sprint1.Task2.V19/Program.cs
+++ Tyuiu.KozachokNA.Sprint1.Task2.V19/Program.cs
@@ -38,7 +38,7 @@ namespace Tyuiu.KozachokNA.Sprint1.Task2.V19
             Console.WriteLine("***************************************************************************");
 
             double bar = Convert.ToDouble(ds.ConvertInchToKm(inch));
-            Math.Round(bar, 2);
+            bar = Math.Round(bar, 3);
 
             Console.WriteLine(bar);
             Console.ReadLine();
